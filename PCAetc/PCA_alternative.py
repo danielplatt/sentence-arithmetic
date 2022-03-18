4872f1594598830309957a6955d49fed45da935f
danielplatt <d.platt@web.de>
2022-03-18 15:07:07 +0000

    Add visualisation with individual distance vectors

--- PCAetc/PCA_alternative.py
+++ PCAetc/PCA_alternative.py
@@ -9,7 +9,8 @@ def get_sentence_embeddings(npy_location):
         assert emb.shape[2]==768
     except AssertionError:
         raise ValueError('File %s has dimension %s in second index. 768 expected.' % (npy_location, emb.shape[2]))
-    return np.reshape(emb, (emb.shape[1], emb.shape[0], 768))
+    emb = np.swapaxes(emb, 0, 1)
+    return emb
 
 def get_differences(emb):
     return emb[0]-emb[1]
@@ -19,7 +20,7 @@ def get_projection_vectors(vecs):
     pca = PCA()
     pca.fit(vecs)
     basis1 = mean
-    basis2 = pca.components_[-1]
+    basis2 = pca.components_[0]
     return basis1, basis2
 
 def project(vec, basis):
@@ -32,21 +33,21 @@ def project(vec, basis):
 
 
 def main():
-    emb = get_sentence_embeddings('data/simple_example_sentences_embedding.npy')
+    emb = get_sentence_embeddings('data/simple_example_sentences_embedding.npy')#[:,:100,:]
     diff = get_differences(emb)
     basis = get_projection_vectors(diff)
-    A_project = np.reshape(np.array([
+    A_project = np.swapaxes(np.array([
         project(vec, basis) for vec in emb[0]
-    ]), (2, -1))
-    B_project = np.reshape(np.array([
+    ]), 0, 1)
+    B_project = np.swapaxes(np.array([
         project(vec, basis) for vec in emb[1]
-    ]), (2, -1))
+    ]), 0, 1)
     mean_diff = np.mean(diff, axis=0)
     mean_diff_project = np.array(project(mean_diff, basis))
 
-    aplot = plt.scatter(*A_project, alpha=0.5)
-    bplot = plt.scatter(*B_project, alpha=0.5)
-    arrowplot = plt.arrow(x=0, y=0, dx=mean_diff_project[0], dy=mean_diff_project[1], width=.0004, facecolor='red')
+    aplot = plt.scatter(*A_project, alpha=0.1)
+    bplot = plt.scatter(*B_project, alpha=0.1)
+    arrowplot = plt.arrow(x=0, y=0, dx=mean_diff_project[0], dy=mean_diff_project[1], width=.003, facecolor='red')
 
     plt.legend((aplot, bplot, arrowplot),
                ('Active sentences', 'Passive sentences', 'Average distance'),
@@ -55,6 +56,32 @@ def main():
                ncol=3,
                fontsize=8)
     plt.savefig('../results/img/active_passive_simple_sentences_visualisation.png')
+    plt.show()
+
+    # showing all difference vectors
+    max_number_of_difference_vectors = 40
+    aplot = plt.scatter(*A_project, alpha=0.1)
+    bplot = plt.scatter(*B_project, alpha=0.1)
+    A_project = np.swapaxes(A_project, 0, 1)
+    B_project = np.swapaxes(B_project, 0, 1)
+    for k, (vec_A, vec_B) in enumerate(zip(A_project, B_project)):
+        if k >= max_number_of_difference_vectors:
+            break
+        arrowplot = plt.arrow(
+            x=vec_A[0],
+            y=vec_A[1],
+            dx=vec_B[0]-vec_A[0],
+            dy=vec_B[1]-vec_A[1],
+            width=.003, facecolor='red',
+        alpha=0.5)
+
+    plt.legend((aplot, bplot, arrowplot),
+               ('Active sentences', 'Passive sentences', 'Corresponding differences'),
+               scatterpoints=1,
+               loc='lower left',
+               ncol=3,
+               fontsize=8)
+    plt.savefig('../results/img/active_passive_simple_sentences_individual_arrows_visualisation.png')
     plt.show()
 
 
