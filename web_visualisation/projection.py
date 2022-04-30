61b9c82433493a8362e9a0ca64dbc1da1d162691
danielplatt <d.platt@web.de>
2022-04-30 21:20:05 +0100

    Add comment for projection wrapper map

--- web_visualisation/projection.py
+++ web_visualisation/projection.py
@@ -5,6 +5,18 @@ from PCAetc.PCA_alternative import project
 
 
 def projection(sentence: str) -> tuple:
+    '''
+    Transforms a sentence into a two-dimensional vector.
+    First, a 768-dimensional embedding of the vector is computed, then this embedding
+    is projected onto the two dimensional subspace of R^768 spanned by the following two vectors:
+    (1) the mean of many vectors of the form A-B, where A and B are corresponding
+    active voice and passive voice sentences;
+    (2) the first principal component of the same vectors A-B
+
+    :param sentence: A string containing a sentenc, such as 'The dog chases the car.'
+    or 'the car is chases by The dog. ' Need not be grammatically correct or meaningful.
+    :return: A two-dimensional vector
+    '''
     model_str = 'all-mpnet-base-v2'
     model = SentenceTransformer(model_str)
     embedding = model.encode(sentence).tolist()
