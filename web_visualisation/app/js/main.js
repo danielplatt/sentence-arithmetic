3535fe3cc6027a384e630f51d315e8f4ef37dc21
Moritz Platt <moritz.platt@kcl.ac.uk>
2022-07-23 20:35:28 +0200

    limit rendered embeddings

--- web_visualisation/app/js/main.js
+++ web_visualisation/app/js/main.js
@@ -1,15 +1,19 @@
+// The limit of pairs to load from the file
+const LIMIT = 100
+
 Papa.parse("data/embeddings.csv", {
   download: true,
   header: true,
   complete: function (raw) {
+    const truncated = raw.data.slice(0, LIMIT - 1);
     const data = {
       datasets: [
         {
           label: "Active",
-          sentences: raw.data.map((element) => {
+          sentences: truncated.map((element) => {
             return element["active_sentence"]
           }),
-          data: raw.data.map((element) => {
+          data: truncated.map((element) => {
             return [element["active_x_coord"], element["active_y_coord"]]
           }),
           borderColor: "red",
@@ -17,10 +21,10 @@ Papa.parse("data/embeddings.csv", {
         },
         {
           label: "Passive",
-          sentences: raw.data.map((element) => {
+          sentences: truncated.map((element) => {
             return element["passive_sentence"]
           }),
-          data: raw.data.map((element) => {
+          data: truncated.map((element) => {
             return [element["passive_x_coord"], element["passive_y_coord"]]
           }),
           borderColor: "blue",
